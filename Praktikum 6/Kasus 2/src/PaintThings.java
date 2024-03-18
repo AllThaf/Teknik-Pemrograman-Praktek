ff1671386eb3fa97387fdb2a4b36e642216a0771
AllThaf <thafafadillah@gmail.com>
2024-03-18 12:48:35 +0700

    fix: polimorphism

--- Praktikum 6/Kasus 2/src/PaintThings.java
+++ Praktikum 6/Kasus 2/src/PaintThings.java
@@ -18,19 +18,21 @@ public class PaintThings {
         final double COVERAGE = 350;
         Paint paint = new Paint(COVERAGE);
 
+        Shape[] shape = new Shape[3];
+
         // Instantiate deck to be a 20 by 35 foot rectangle
-        Rectangle deck = new Rectangle(20, 35);
+        shape[0] = new Rectangle(20, 35); // deck
         // Instantiate bigBall to be a sphere of radius 15
-        Sphere bigBall = new Sphere(15);
+        shape[1] = new Sphere(15); // bigBall
         // Instantiate tank to be a cylinder of radius 10 and height 30
-        Cylinder tank = new Cylinder(10, 30);
+        shape[2] = new Cylinder(10, 30); // tank
 
         double deckAmt, ballAmt, tankAmt;
         // Make the appropriate method calls to assign the correct values to the three
         // amount variables.
-        deckAmt = paint.amount(deck);
-        ballAmt = paint.amount(bigBall);
-        tankAmt = paint.amount(tank);
+        deckAmt = paint.amount(shape[0]);
+        ballAmt = paint.amount(shape[1]);
+        tankAmt = paint.amount(shape[2]);
 
         // Instantiate the three shapes to paint
         // Compute the amount of paint needed for each shape
